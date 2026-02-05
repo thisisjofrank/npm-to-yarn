0ed6d30241d64144d34ecbf78dc7812c7e5fced1
Jo Franchetti <jofranchetti@gmail.com>
2026-02-05 14:56:06 +0000

    Apply suggestion from @thisisjofrank

--- src/npmToDeno.ts
+++ src/npmToDeno.ts
@@ -133,7 +133,7 @@ export function npmToDeno(_m: string, command: string): string {
     case 'test':
     case 't':
     case 'tst':
-      converted = 'deno task test'
+      converted = 'deno test'
       break
 
     case 'init':
